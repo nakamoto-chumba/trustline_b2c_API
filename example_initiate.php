d2f07d814013e7951e2f0377fbe072d20ba9f1dd
nakamoto-chumba <163315308+nakamoto-chumba@users.noreply.github.com>
2024-09-23 00:29:59 +0300

    Update example_initiate.php

--- example_initiate.php
+++ example_initiate.php
@@ -1,5 +1,15 @@
 <?php
 $curl = curl_init();
+
+$data = array(
+    "amount" => 10,
+    "phone_number" => "0798988787",
+    "api_key" => "3A6anVoWFZrRk5qSVl0MGNMOERGMlR3dlhrQ0VWUWJHNDVVVnNaMEdDSw",
+    "network_code" => "1",
+    "external_reference" => "INV-99992",
+    "callback_url" => "https://example.com/callback.php"
+);
+
 curl_setopt_array($curl, array(
   CURLOPT_URL => 'https://trustline.co.ke/api/v2/payments',
   CURLOPT_RETURNTRANSFER => true,
@@ -9,18 +19,19 @@ curl_setopt_array($curl, array(
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS =>'{
-    "amount": 10,
-    "phone_number": "0798988787",
-    "api_key": "3A6anVoWFZrRk5qSVl0MGNMOERGMlR3dlhrQ0VWUWJHNDVVVnNaMEdDSw", 
-    "network_code":"1",
-    "external_reference": "INV-99992",
-    "callback_url": "https://example.com/callback.php"
-}',
+  CURLOPT_POSTFIELDS => json_encode($data),
   CURLOPT_HTTPHEADER => array(
     'Content-Type: application/json',
   ),
 ));
+
 $response = curl_exec($curl);
+
+if(curl_errno($curl)) {
+    // Handle cURL error
+    echo 'Error: ' . curl_error($curl);
+} else {
+    echo $response;
+}
+
 curl_close($curl);
-echo $response;
